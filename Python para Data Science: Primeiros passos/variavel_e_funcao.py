5d8df594d4d977a51aeb389935bddb417e9577da
MateusRCosta <86742685+MateusRCosta@users.noreply.github.com>
2021-07-13 19:11:13 -0300

    Create variavel_e_funcao.py

---
+++ Python para Data Science: Primeiros passos/variavel_e_funcao.py
@@ -0,0 +1,14 @@
+# Criação de variável e de uma função simples
+# O algoritmo consiste somar as variáveis  e mostrar o resultado depois.
+
+# definindo os valores das variáveis
+variavel_0 = 1 
+variavel_1 = 2
+
+# definindo a função que calculará as variáveis e mostrará o resultado
+def soma(): 
+  resultado = variavel_0 + variavel_1 
+  print(resultado) 
+  
+# utilizando a função criada    
+soma()
