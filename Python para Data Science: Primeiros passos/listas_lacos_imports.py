d42b1d482402841cd80a01e2d746ba6fd8e098e5
MateusRCosta <86742685+MateusRCosta@users.noreply.github.com>
2021-07-14 12:07:20 -0300

    Create listas_lacos_imports.py

---
+++ Python para Data Science: Primeiros passos/listas_lacos_imports.py
@@ -0,0 +1,20 @@
+# O objetivo do algoritmo é gerar números aleatórios referente a idade, e a partir deles dizer se é maior de idade ou não.
+# 
+# OBS : a idade de referência para a maioridade é de 18 anos.
+
+# importando uma função específica da biblioteca random.
+from random import randrange
+
+# criação de uma lista vazia para guardar as idades geradas posteriormente
+idades = []
+
+# criação de um laço para preencher a lista criada a partir da função randrange retirada da biblioteca random
+for idade in range(1,5):
+  idades.append(randrange(0, 118))
+
+# criação de um laço para definir quais idades são consideradas maiores de 18 anos  
+for idade in idades:
+  if(idade < 18):
+    print('{} anos e é menor de idade.'.format(idade))
+  else:
+    print('{} anos e é maior de idade.'.format(idade))
